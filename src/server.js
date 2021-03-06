37ba8e37dbe7c49a3678189ea0a9e5a2e8f1aa22
Lucas Dutra <ldutra98@gmail.com>
2021-03-06 13:47:37 -0300

    ci: Add lint step ci
    Co-authored-by: Youssef Muhamad <emaildeyoussefmuhamad@gmail.com>

--- src/server.js
+++ src/server.js
@@ -1,14 +1,13 @@
-import express from 'express'
+import express from 'express';
 
-const app = express()
+const app = express();
 
 app.get('/', (req, res) => {
-    res.send('yossfe')
-})
+  res.send('yossfe');
+});
 
 const HOST = 'localhost';
 const PORT = process.env.PORT || 3002;
 app.listen(PORT);
 
 console.log(`Running at ${HOST}:${PORT}`);
-
