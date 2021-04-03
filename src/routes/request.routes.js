849b691392b3ed33f981607eef76aef90eb622f3
Matheus Afonso <matheusafonsouza@gmail.com>
2021-04-02 23:41:45 -0300

    feat: fga-eps-mds/2020.2-Lend.it#121 create update lender and finalize request methods
    Co-authored-by: Mateus Maia <mateusmaiamaia@hotmail.com>

--- src/routes/request.routes.js
+++ src/routes/request.routes.js
@@ -9,6 +9,9 @@ requestRouter.post('/request', Request.createRequest);
 requestRouter.put('/request/:id', Request.updateRequest);
 requestRouter.delete('/request/:id', Request.deleteRequest);
 
+requestRouter.patch('/request/:id', Request.updateLender);
+requestRouter.patch('/request/:id/finalize', Request.finalizeRequest);
+
 requestRouter.get('/product-category', Request.getProductCategory);
 requestRouter.post('/product-category', Request.createProductCategory);
 
