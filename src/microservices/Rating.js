21a05dad85cebc42cc67a6b4a2ee534c9c969116
mateuscunhamaia <mateusmaiamaia@hotmail.com>
2021-04-15 18:25:09 -0300

    feat: fga-eps-mds/2020.2-Lend.it#136 connect rating routes in microservice
    Co-authored-by: Vinicius Saturnino <viniciussaturnino78@gmail.com>
    Co-authored-by: Matheus Afonso <matheusafonsouza@gmail.com>

--- src/microservices/Rating.js
+++ src/microservices/Rating.js
@@ -1,5 +1,12 @@
+import { basePostRating } from '../utils/baseRequest.js';
 import endpoint from '../utils/endpoint.js';
 
 const { rating } = endpoint[process.env.NODE_ENV] || endpoint.fallback;
 
-export default {};
+async function createRating(req, res) {
+  res.json(await basePostRating(`${rating}/rating`, req.body, req.headers));
+}
+
+export default {
+  createRating,
+};
